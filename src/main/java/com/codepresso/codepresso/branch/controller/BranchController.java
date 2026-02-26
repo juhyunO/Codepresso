55b0bf98b724f0fd03bd006d310afea1916e2b55
juhyun <guswnfl5@naver.com>
2026-02-26 10:57:26 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/codepresso/codepresso/branch/controller/BranchController.java
+++ src/main/java/com/codepresso/codepresso/branch/controller/BranchController.java
@@ -1,13 +1,9 @@
 package com.codepresso.codepresso.branch.controller;
 
-import com.codepresso.codepresso.branch.dto.request.BranchSearchRequest;
 import com.codepresso.codepresso.branch.dto.response.BranchInfoResponse;
-import com.codepresso.codepresso.branch.dto.response.BranchListResponse;
 import com.codepresso.codepresso.branch.service.BranchService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
